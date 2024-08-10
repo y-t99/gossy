58dbb483495c2113ce1adb888d0682094f23b287
Itou Ng <13060889537@sina.cn>
2024-08-10 21:07:08 +0800

    feat(web-server): chat windows layout

--- chat-room-web-server/src/App.tsx
+++ chat-room-web-server/src/App.tsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
-    <div className="bg-slate-300 font-mono">
-      🍀 Hello World!
+    <div className="bg-slate-300 font-mono w-screen h-screen flex items-center justify-center">
+      <div className="bg-white size-7/12">
+        🍀 Hello World!
+      </div>
     </div>
   )
 }
