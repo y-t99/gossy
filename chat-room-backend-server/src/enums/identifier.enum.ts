56dbabbcd80e1b6ab9b45d8fd6e046f370e8a02c
itou ng <13060889537@sina.cn>
2024-12-13 17:50:35 +0800

    feat: enhance identifier handling and add ID generation utility

--- chat-room-backend-server/src/enums/identifier.enum.ts
+++ chat-room-backend-server/src/enums/identifier.enum.ts
@@ -3,6 +3,9 @@
  */
 export enum Identifier {
   USER = 'm.id.user',
+  ROOM = 'm.id.room',
+  EVENT = 'm.id.event',
+  ROOM_ALIAS = 'm.id.room_alias',
   THIRDPARTY = 'm.id.thirdparty',
   PHONE = 'm.id.phone',
 }
---
+++ chat-room-backend-server/src/utils/id-generator.util.ts
@@ -0,0 +1,53 @@
+/// @see https://spec.matrix.org/v1.12/appendices/#common-identifier-format
+
+import { Identifier } from '../enums';
+import { customAlphabet } from 'nanoid';
+
+const customIdGenerator = customAlphabet(
+  '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  16,
+);
+
+function testUserIdPart(localpart: string): boolean {
+  return /^[a-zA-Z0-9-_]+$/.test(localpart);
+}
+
+const sigil = {
+  [Identifier.USER]: '@',
+  [Identifier.ROOM]: '!',
+  [Identifier.EVENT]: '$',
+  [Identifier.ROOM_ALIAS]: '#',
+};
+
+const maxIdByteLength = 255;
+
+export function isValidId(
+  type: Identifier,
+  id: string,
+  domain: string,
+): boolean {
+  if (type === Identifier.USER) {
+    const isValid = testUserIdPart(id);
+    if (!isValid) return false;
+  } else if (
+    type !== Identifier.ROOM &&
+    type !== Identifier.ROOM_ALIAS &&
+    type !== Identifier.EVENT
+  ) {
+    return false;
+  }
+
+  if (Buffer.byteLength(`${sigil[type]}${id}:${domain}`) > maxIdByteLength) {
+    return false;
+  }
+
+  return true;
+}
+
+export function generateId(type: Identifier.ROOM | Identifier.EVENT) {
+  if (type !== Identifier.ROOM && type !== Identifier.EVENT) {
+    throw new Error('Invalid identifier type');
+  }
+
+  return `${sigil[type]}${customIdGenerator()}`;
+}
